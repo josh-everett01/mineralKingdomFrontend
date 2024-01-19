033bb9e64a4c49525df15c23573c04b8d77e9f01
Joshua Everett <59103702+josh-everett01@users.noreply.github.com>
2024-01-19 01:42:37 -0600

    Set public host to prevent Invalid Host header error

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,10 @@
 module.exports = {
   devServer: {
-    port: process.env.PORT || 8080  // Use the PORT environment variable, default to 8080 if not set
+    port: process.env.PORT || 8080,  // Use the PORT environment variable, default to 8080 if not set
+    public: 'mineralkingdomfrontend.onrender.com',  // Set the public host
+    disableHostCheck: true,  // Optionally, you can disable host check (use with caution)
+    headers: {
+      'Access-Control-Allow-Origin': '*'  // If you're dealing with CORS issues
+    }
   }
 };
